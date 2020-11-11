5d13e977c4cc3fb48101db7591ed13957338b0c6
Kundan Kumar <kundan.kumar01@nagarro.com>
2020-11-11 09:26:34 +0530

    Functionality part in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube-ang';
+  todoValue:string;
+  list:Todo[];
+  ngOnInit(){
+    this.list= [];
+    this.todoValue = "";
+  }
+  addItem(){
+    if(this.todoValue !== ""){
+      const newItem: Todo={
+        id: Date.now(),
+        value:this.todoValue,
+        isDone:false
+      }
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id:number){
+      this.list = this.list.filter(item =>item.id !==id);
+  }
+
+
 }
